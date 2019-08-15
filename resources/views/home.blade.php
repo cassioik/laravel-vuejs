9447c35afa7f12f28c5acddb59240dfe3d0e0d53
Cássio Izumi Krüger <cassiokruger@gmail.com>
2019-08-14 23:17:10 -0300

    Componente Topo e Painel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <painel titulo="Dashboard">
+                    Teste de conteúdo...
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="row">
+                        <div class="col-md-4">
+                            <painel titulo="Conteúdo 1">
+                                    Teste de conteúdo...
+                            </painel>
                         </div>
-                    @endif
+                        <div class="col-md-4">
+                            <painel titulo="Conteúdo 2">
+                                    Teste de conteúdo...
+                            </painel>
+                        </div>
+                        <div class="col-md-4">
+                            <painel titulo="Conteúdo 3">
+                                    Teste de conteúdo...
+                            </painel>
+                        </div>
+                    </div>
 
-                    You are logged in!
-                </div>
+                </painel>
             </div>
         </div>
     </div>
-</div>
 @endsection
